0960aa996ce5c902be1825913e89997c044eefc4
Lyon <lozybean@163.com>
2015-08-05 18:48:51 +0800

    init 20150805

---
+++ 16S/16S_work.sh
@@ -0,0 +1,18 @@
+usr_config=16S_usr_config.sh
+source $usr_config
+
+
+#01_pick otu
+source $pipeline_path/01_pick_otu.sh
+
+#02_alpha_rare_curve
+source $pipeline_path/02_alpha_rare_curve.sh
+
+#03_otu_table (downsize)
+source $pipeline_path/03_otu_table.sh
+
+#04_diversity_analysis
+source $pipeline_path/04_diversity_analysis.sh
+
+#05_tax_diff_analysis
+source $pipeline_path/05_diff_taxa_analysis.sh
---
+++ README.md
@@ -0,0 +1,22 @@
+requirement:
+
+1. python packages:
+	
+	biopython >1.65
+	numpy >1.9.2
+	qiime >1.9.1  installed natively with a full install
+
+2. R packages:
+	
+	R  configure :  --enable-R-shlib --enable-BLAS-shlib --with-blas --with-lapack --with-libtiff
+	NMF
+	RColorBrewer
+	WGCNA
+	ade4
+	gplots
+	ggplot2
+	VennDiagram
+	
+	may some other packages
+
+3. Perl
